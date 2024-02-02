10977146de1cc67c113a072656db248bc88c85d5
Dylan <dylanalifu@Dylan.local>
2024-02-02 15:12:01 -0800

    Updated checkbox

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -51,6 +51,7 @@ const TaskItem = ({ task, setTaskToBeDeleted }) => {
             />
           ) : (
             <>
+              <div class="container">
               <input
                 className="form-check-input me-1"
                 type="checkbox"
@@ -58,6 +59,14 @@ const TaskItem = ({ task, setTaskToBeDeleted }) => {
                 checked={isCompleted}
                 onChange={handleChange}
               ></input>
+                <label for={taskId} class="check">
+                  <svg width="18px" height="18px" viewBox="0 0 18 18">
+                    <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
+                    <polyline points="1 9 7 14 15 4"></polyline>
+                  </svg>
+                </label>
+              </div>
+              
               <label
                 className={`form-check-label ${isCompleted ? "completed" : ""}`}
                 htmlFor={taskId}
